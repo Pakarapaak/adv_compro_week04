3ef353d0f62e316d599a52f9457a44579408d4dd
Pakarapaak <67011221@kmitl.ac.th>
2025-07-22 15:04:34 +0700

    add string operation and import to use in main

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
-""" #Ex1
+""" #Ex2
 import calculator
 print(calculator.add(100,69))
 print(calculator.subtrack(100,69))
 print(calculator.multiply(100,69))
 print(calculator.divide(100,69)) """
 
-#Ex2
+""" #Ex3
 import lambda_calculator
 print(lambda_calculator.add(100,69))
 print(lambda_calculator.subtrack(100,69))
 print(lambda_calculator.multiply(100,69))
-print(lambda_calculator.divide(100,69))
+print(lambda_calculator.divide(100,69)) """
+
+#Ex4
+import string_operations as so
+sample_string = "hello World"
+print("Original:", sample_string)
+print("Reversed:", so.reverse_string(sample_string))
+print("Capitalized:", so.capitalize_string(sample_string))
+print("Lowercase:", so.lowercase_string(sample_string))
+print("Uppercase:", so.uppercase_string(sample_string))
