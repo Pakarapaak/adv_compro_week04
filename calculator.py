b889ce153c921371a83be27107105e7262d95d4e
Pakarapaak <67011221@kmitl.ac.th>
2025-07-22 14:44:37 +0700

    add calculator and try use in main

---
+++ calculator.py
@@ -0,0 +1,5 @@
+def add(x,y):
+    return x+y
+subtrack = lambda x,y: x-y
+multiply = lambda x,y: x*y
+divide = lambda x,y: x/y
---
+++ main.py
@@ -0,0 +1,6 @@
+import calculator
+
+print(calculator.add(100,69))
+print(calculator.subtrack(100,69))
+print(calculator.multiply(100,69))
+print(calculator.divide(100,69))
