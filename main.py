b6fc9b6fb9e422010f70d30b4c6039c8948762c3
Pakarapaak <67011221@kmitl.ac.th>
2025-07-22 14:53:53 +0700

    import lambada and use in main

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+""" #Ex1
 import calculator
-
 print(calculator.add(100,69))
 print(calculator.subtrack(100,69))
 print(calculator.multiply(100,69))
-print(calculator.divide(100,69))
+print(calculator.divide(100,69)) """
+
+#Ex2
+import lambda_calculator
+print(lambda_calculator.add(100,69))
+print(lambda_calculator.subtrack(100,69))
+print(lambda_calculator.multiply(100,69))
+print(lambda_calculator.divide(100,69))
